7fd300276853ed62f24d349485f27ade75830406
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-04 21:54:34 +0530

    Update Solution.sql

--- Revising the Select Query I/Solution.sql
+++ Revising the Select Query I/Solution.sql
@@ -1,1 +1,7 @@
 select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
+
+select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
+select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
+select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
+select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
+select * from CITY where POPULATION > 100000 and COUNTRYCODE = 'USA';
