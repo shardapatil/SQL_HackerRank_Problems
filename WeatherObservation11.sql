a18016b0a5eb72191ba4042748686fdffdc6f3ab
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-10 22:49:26 +0530

    Update WeatherObservation11.sql

--- WeatherObservation11.sql
+++ WeatherObservation11.sql
@@ -1,1 +1,2 @@
- select distinct city from station where city not regexp '^[aeiou]' or city not regexp '[aeiou]$';
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+select distinct city from station where city not regexp '^[aeiou]' or city not regexp '[aeiou]$';
